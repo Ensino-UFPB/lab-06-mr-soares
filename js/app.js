fca5ccb412d8ed0dc8c41196a4745f1e7bca2930
mr-soares <prrs321paulo@gmail.com>
2025-02-26 14:50:00 -0300

    lab 6

--- js/app.js
+++ js/app.js
@@ -1,24 +1,9 @@
-import { valida } from './validacao.js';
+import { valida } from './validacao.js'
 
-// Configuração da máscara monetária
-const optionsBRL = {
-    prefix: 'R$ ',
-    suffix: '',
-    fixed: true,
-    fractionDigits: 2,
-    decimalSeparator: ',',
-    thousandsSeparator: '.',
-    cursor: 'end'
-};
+const inputs = document.querySelectorAll('input')
 
-// Aplicar a máscara ao campo de preço
-const precoInput = document.getElementById('preco');
-SimpleMaskMoney.setMask(precoInput, optionsBRL);
-
-// Aplicar validações a todos os campos
-const inputs = document.querySelectorAll('input');
 inputs.forEach(input => {
     input.addEventListener('blur', (evento) => {
-        valida(evento.target);
-    });
-});
+        valida(evento.target)
+    })
+})
